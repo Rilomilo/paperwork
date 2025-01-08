ef923de7b8b63999ed44f0ac5ec52b02e35cbbac
Rilomilo <269797689@qq.com>
2025-01-08 09:25:10 +0800

    add sam lora

--- models/__init__.py
+++ models/__init__.py
@@ -5,8 +5,9 @@ import torchinfo
 from .unet import U_Net
 from .common import load_checkpoint
 from .segment_anything import build_sam_vit_b
+from .segment_anything.sam_lora import LoRA_Sam
 
-def get_model(name, output_ch, weights=None):
+def get_model(name, output_ch, weights=None, rank=None):
     if name=="U_Net":
         model=U_Net(img_ch=3, output_ch=output_ch)
     elif name=="u-resnet34":
@@ -19,6 +20,9 @@ def get_model(name, output_ch, weights=None):
         model.segmentation_head.add_module("softmax", nn.Softmax(dim=1))
     elif name=="sam":
         model = build_sam_vit_b(output_ch, "weights/sam_vit_b_01ec64.pth")
+    elif name=="sam_lora":
+        model = build_sam_vit_b(output_ch, "weights/sam_vit_b_01ec64.pth")
+        sam_lora=LoRA_Sam(model, r=rank)
     else:
         raise ValueError(f"Model {name} not found")
     
